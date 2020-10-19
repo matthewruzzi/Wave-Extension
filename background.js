809a0376e534dcdd95fd25b06cdfd6448283e44a
mattruzzi <68619790+mattruzzi@users.noreply.github.com>
2020-10-19 14:59:39 -0700

    Added welcome page

---
+++ background.js
@@ -0,0 +1,13 @@
+browser.runtime.onInstalled.addListener(async ({ reason, temporary }) => {
+    if (temporary) return; // skip during development
+    switch (reason) {
+        case "install":
+            {
+                const url = browser.runtime.getURL("welcome/wave-installed.html");
+                await browser.tabs.create({ url });
+                // or: await browser.windows.create({ url, type: "popup", height: 600, width: 600, });
+            }
+            break;
+        // see below
+    }
+});
